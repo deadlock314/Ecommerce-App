b95edc3f9b9646c4d6e0260127ef65b46513a5eb
chandan soni <chandansoni2270@gmail.com>
2022-03-06 19:59:46 +0530

    changed tab order in bottom navBar

--- Components/NavComponents/BottonNavBar.js
+++ Components/NavComponents/BottonNavBar.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import Cart from '../CartComponent/Cart';
 import Profile from '../ProfileComponent/Profile';
 import Home from '../HomeComponent/Home';
-import Search from '../SearchBarComponent/SearchBar';
+import Search from '../SearchBarComponent/Search';
 import Notifi from '../NotiComponent/Notification';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -15,20 +15,22 @@ const BottomNavBar=()=> {
         <NavigationContainer >
                 <Tab.Navigator 
                    screenOptions={({ route }) => ({
-                   tabBarIcon: () => {
+                   tabBarActiveTintColor:'#00f',
+                   tabBarIcon: ({focused}) => {
                    let iconName;
                    if (route.name === 'profile')
-                      iconName = 'happy';
+                      iconName =  (focused)?'person':'person-outline';
                    else
-                       iconName=route.name
-        return <Ionicons name={iconName} size={35} color='#000' />;  },
-          tabBarShowLabel:false,
-          tabBarActiveTintColor:'#00f'
+                       iconName= (focused)?route.name:`${route.name}-outline`
+                       
+        return <Ionicons name={iconName} size={30} color={(focused)?'#165191':'#000'} />;  },
+
+          tabBarShowLabel:false,headerShown:false
         })}>
                 <Tab.Screen name='home' component={Home}/>
-                <Tab.Screen name='cart'  component={Cart} />
-                <Tab.Screen name='search' component={Search}/>
                 <Tab.Screen name='notifications'component={Notifi}/>
+                <Tab.Screen name='search' component={Search}/>
+                <Tab.Screen name='cart'  component={Cart} />
                 <Tab.Screen name='profile' component={Profile}/>
                 
                 
