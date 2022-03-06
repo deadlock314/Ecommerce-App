babe579f7dfe6d854a5ceb0a87507dd517a6b79e
chandan soni <chandansoni2270@gmail.com>
2022-03-06 19:55:43 +0530

    added custom api requests(get,post)

---
+++ ApiRequests/APIReqHandler.js
@@ -0,0 +1,42 @@
+
+const GetReq=async(url)=> {
+
+    try{
+        const res= await fetch(url,
+            {
+                method:"GET",credentials:'include',
+                headers:{
+                Accept:"application/json",
+                'Content-Type':'application/json'
+            }
+        });
+        const resJson= await res.json();
+         return resJson
+
+    } catch (err) {
+        console.log(err);
+        return {isError:true}
+    } 
+}
+const Postreq= async(url,data)=>{
+
+    try{
+        const res= await fetch(url
+        ,{
+            method:"POST",credentials:'include',
+            headers:{
+                Accept:"application/json",
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(data)
+        })
+        const resJson= await res.json();
+        return resJson
+
+    } catch(err){
+        return {isError:tue}
+    }
+
+}
+
+export {GetReq,Postreq};
