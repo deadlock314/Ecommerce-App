7b7759363ae8be3347c816c1f3fd9a626392c936
chandan soni <chandansoni2270@gmail.com>
2022-03-10 13:35:05 +0530

    linked redux with app

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
+import { Provider } from 'react-redux';
 import BottomNavBar from './Components/NavComponents/BottonNavBar';
+import store from './ReduxCode/Store';
 
 
 const App=()=> {
   
   return (
-   
-    <BottomNavBar/>
+   <Provider store={store}>
+     <BottomNavBar/>
+   </Provider>
+    
   );
 }
 
