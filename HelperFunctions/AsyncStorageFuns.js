d817fe291ab9a71047cfae5fb3ecbbc415dbdf59
chandan soni <chandansoni2270@gmail.com>
2022-03-10 13:43:33 +0530

    added get and set functions for storing and fetching product data from  devices's storage

---
+++ HelperFunctions/AsyncStorageFuns.js
@@ -0,0 +1,22 @@
+import AsyncStorageLib from '@react-native-async-storage/async-storage';
+
+const getAsyncStorage=async(key)=>{
+    try{
+        const data= await AsyncStorageLib.getItem(key);
+        console.log(JSON.parse(data));
+        return JSON.parse(data);
+    } catch (error) {
+        return false
+    }
+   
+}
+
+const setAsyncStorage=async(key,val)=>{
+    try{
+      await AsyncStorageLib.setItem(key,JSON.stringify(val));
+    } catch (error) {
+        return false
+    }
+}
+
+export {getAsyncStorage ,setAsyncStorage};
