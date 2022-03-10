a4b59b95829099fcc4eeb63c2d55894da37a78fa
chandan soni <chandansoni2270@gmail.com>
2022-03-10 13:40:44 +0530

    added redux code for add to cart btn

--- Components/ProductComponent/ProductInfo.js
+++ Components/ProductComponent/ProductInfo.js
@@ -1,53 +1,79 @@
-import { Text,StyleSheet ,ScrollView,Image,View } from 'react-native';
+import { Text, StyleSheet, ScrollView, Image, View } from 'react-native';
 import ProductExtendedDes from './ProductExtendedDes';
-import {CustomInfoBtn} from '../CustomComponent/CustomBtn';
-const ProductInfo=({navigation,route})=> {
-    const product=route.params;
+import { CustomInfoBtn } from '../CustomComponent/CustomBtn';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateCartData, updatePrice } from '../../ReduxCode/Reducers';
+import { priceAdder } from '../../HelperFunctions/priceAdder';
 
-    const AddtoCartHandler=()=>navigation.navigate('cartHome',{screen:'cart',  ...product})
-    const BuyNowHandler=()=>navigation.navigate('cart',{screen:'cartHome',...product})
+const ProductInfo = ({ navigation, route }) => {
+    const product = route.params;
+
+    const CartData = useSelector((s) => s.cartData);
+    const dispatch = useDispatch();
+
+    const AddtoCartHandler = () => {
+
+        const FindDuplicate = () => {
+            for (let i = 0; i < CartData.prevCartData.length; i++)
+                if (product._id == CartData.prevCartData[i]._id)
+                    return false;
+            return true
+        }
+        if (FindDuplicate()){
+
+            dispatch(updateCartData({ ...product }));
+            dispatch(updatePrice({
+                totalPrice:priceAdder(CartData.totalPrice,product.price,'add'),
+                countObj:{id:product._id,count:1}
+            }));
+        }
+        navigation.navigate('cartHome')
+    }
+
+
+    const BuyNowHandler = () => navigation.navigate('cart', { screen: 'cartHome', ...product })
 
     return (
         <ScrollView>
-                <View style={{backgroundColor:'#fff'}}>
-                <Image style={{...styles.productImage,width:"90%",height:250}} source={{uri:product.imgLink}} />
-                <Text  style={styles.productDes}>{product.des}</Text>
-                <Text  style={styles.productPrice}>{product.price}</Text>
+            <View style={{ backgroundColor: '#fff' }}>
+                <Image style={{ ...styles.productImage, width: "90%", height: 270 }} source={{ uri: product.imgLink }} />
+                <Text style={styles.productDes}>{product.des}</Text>
+                <Text style={styles.productPrice}>{product.price}</Text>
 
                 <View style={styles.productBtn}>
-                    <CustomInfoBtn prop={{btnTitle:"Add to Cart",onPressFun:AddtoCartHandler}} />
-                    <CustomInfoBtn prop={{btnTitle:"Buy Now", onPressFun:BuyNowHandler}} />
+                    <CustomInfoBtn prop={{ btnTitle: "Add to Cart", onPressFun: AddtoCartHandler }} />
+                    <CustomInfoBtn prop={{ btnTitle: "Buy Now", onPressFun: BuyNowHandler }} />
                 </View>
-                </View>
-                <ProductExtendedDes prop={{productId:product.productId,productType:product.type}}/>
-        </ScrollView> 
-        
-     );
+            </View>
+            <ProductExtendedDes prop={{ productId: product.productId, productType: product.type }} />
+        </ScrollView>
+
+    );
 }
 
-const styles=StyleSheet.create({
-    productImage:{
-        alignSelf:'center',
-        marginTop:60,
-        marginBottom:10,
-        resizeMode:'stretch'
-     },
-  
-     productDes:{
-         fontSize:19,
-         marginLeft:10
-     },
-     productPrice:{
-         fontSize:26,
-         marginLeft:25,
-         marginBottom:14
-     },
-     productBtn:{
-         flexDirection:'row',
-         justifyContent:'space-around',
-         marginBottom:20
-        
-     }
+const styles = StyleSheet.create({
+    productImage: {
+        alignSelf: 'center',
+        marginTop: 60,
+        marginBottom: 10,
+        resizeMode: 'stretch'
+    },
+
+    productDes: {
+        fontSize: 19,
+        marginLeft: 10
+    },
+    productPrice: {
+        fontSize: 26,
+        marginLeft: 25,
+        marginBottom: 14
+    },
+    productBtn: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginBottom: 20
+
+    }
 })
 
 export default ProductInfo;
